7de0cc08b39c8a4ab05990ed8d1e95b8df3e5e5c
gapima <gapima7@gmail.com>
2024-04-26 10:42:59 -0300

    /home/gapima

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@ int	main(int ac, char **ag, char **envp)
 	if (check_empty(ag[2]) || check_empty(ag[3]))
 		return (1);
 	f1 = open(ag[1], O_RDONLY);
-	f2 = open(ag[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	f2 = open(ag[4], O_CREAT | O_RDWR | O_TRUNC, 0777);
 	if (f1 < 0)
 		return (ft_putstr(strerror(errno), ag[1]));
 	if (f2 < 0)
